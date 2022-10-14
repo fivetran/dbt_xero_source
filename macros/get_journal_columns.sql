3ac088db4c613d0032ca0089b9615677417f5437
Sheri Nguyen <sheri.nguyen@fivetran.com>
2022-10-13 16:10:55 -0800

    Updates for dbt-utils to dbt-core cross-db macro migration

--- macros/get_journal_columns.sql
+++ macros/get_journal_columns.sql
@@ -1,14 +1,14 @@
 {% macro get_journal_columns() %}
 
 {% set columns = [
-    {"name": "_fivetran_synced", "datatype": dbt_utils.type_timestamp()},
-    {"name": "created_date_utc", "datatype": dbt_utils.type_timestamp()},
+    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
+    {"name": "created_date_utc", "datatype": dbt.type_timestamp()},
     {"name": "journal_date", "datatype": "date"},
-    {"name": "journal_id", "datatype": dbt_utils.type_string()},
-    {"name": "journal_number", "datatype": dbt_utils.type_int()},
-    {"name": "reference", "datatype": dbt_utils.type_string()},
-    {"name": "source_id", "datatype": dbt_utils.type_string()},
-    {"name": "source_type", "datatype": dbt_utils.type_string()}
+    {"name": "journal_id", "datatype": dbt.type_string()},
+    {"name": "journal_number", "datatype": dbt.type_int()},
+    {"name": "reference", "datatype": dbt.type_string()},
+    {"name": "source_id", "datatype": dbt.type_string()},
+    {"name": "source_type", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
